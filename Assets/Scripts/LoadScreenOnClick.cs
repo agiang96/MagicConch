c36375192ac6368935d5bd4d53d094abb1087c36
Alexander Giang <alexgiang96@gmail.com>
2018-04-16 19:14:44 -0700

    Adding Gameplay/MagicConch scene along with tetx edits in the scenes to make them visible

---
+++ Assets/Scripts/LoadScreenOnClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//taken from:
+//https://unity3d.com/learn/tutorials/topics/user-interface-ui/creating-main-menu
+public class LoadScreenOnClick : MonoBehaviour {
+
+    /* ****************************************
+     * Function: LoadByIndex
+     * ****************************************
+     * If you put this method on a button, 
+     * Loads a Scene according to the Index
+     * according to the Build
+     * ****************************************
+     */
+	public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex); 
+    }
+}
---
+++ Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//taken from 
+//https://unity3d.com/learn/tutorials/topics/user-interface-ui/creating-main-menu
+public class QuitOnClick : MonoBehaviour {
+     
+    /* ****************************************
+     * Function: Quit
+     * ****************************************
+     * If you put this method on a button, 
+     * It will quit the application, unless 
+     * you're in the Editor
+     * ****************************************
+     */
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); 
+#endif
+    }
+}
