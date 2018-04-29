c7aaec61330c1d6897ae3d7a469da82f21fc629e
chingyi <alisa.cy.lui@gmail.com>
2018-04-28 21:37:39 -0700

    update the bounce script of the shell

--- Assets/Scripts/UI/BounceConch.cs
+++ Assets/Scripts/UI/BounceConch.cs
@@ -9,15 +9,16 @@ public class BounceConch : MonoBehaviour {
     //adjust this to change how high it goes
     float height = 0.4f;
 
-   
+
 
     void Update()
     {
         //get the objects current position and put it in a variable so we can access it later with less code
         Vector2 pos = transform.position;
+//        Debug.Log(pos);
         //calculate what the new Y position will be
         float newY = Mathf.Sin(Time.time * speed);
         //set the object's Y to the new calculated Y
-        transform.position = new Vector2(pos.x, newY) * height;
+        transform.position = new Vector2(pos.x, newY*height);
     }
 }
